2561f05d4750035210937480a0765c82eca86916
ArpanSankesh <arpansankesh830@gmail.com>
2025-12-29 00:07:14 +0530

    Final Commit before REVISION

--- server/server.js
+++ server/server.js
@@ -16,12 +16,28 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 connectCloudinary();
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',                   
+  'https://fit-fin-tech.vercel.app'         
+];
 
-// API Endpoints
-app.use('/api/auth', authRouter); 
+app.use(cors({
+  origin: function (origin, callback) {
+    
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/api/user', authRouter); 
 app.use('/api/blogs', blogRouter);
 
 app.get('/', (req, res) => {
